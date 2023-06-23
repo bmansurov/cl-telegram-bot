a86d1135fa8aadf0f4471ae143bf40cb93c0b053
bm <b@m.z>
2023-06-22 23:44:30 -0400

    Get rid of the "get-" prefix from the USER slot readers

--- src/user.lisp
+++ src/user.lisp
@@ -9,46 +9,46 @@
                 #:raw-data-to-initargs)
   (:export #:user
            #:make-user
-           #:get-user-id
-           #:get-user-is-bot
-           #:get-user-first-name
-           #:get-user-last-name
-           #:get-user-username
-           #:get-user-language-code
-           #:get-user-is-premium
-           #:get-user-added-to-attachment-menu
-           #:get-user-can-join-groups
-           #:get-user-can-read-all-group-messages
-           #:get-user-supports-inline-queries
-           #:get-raw-data))
+           #:user-id
+           #:user-is-bot
+           #:user-first-name
+           #:user-last-name
+           #:user-username
+           #:user-language-code
+           #:user-is-premium
+           #:user-added-to-attachment-menu
+           #:user-can-join-groups
+           #:user-can-read-all-group-messages
+           #:user-supports-inline-queries
+           #:user-raw-data))
 (in-package cl-telegram-bot/user)
 
 (defclass user ()
   ((id :initarg :id
-       :reader get-user-id)
+       :reader user-id)
    (is-bot :initarg :is-bot
-           :reader get-user-is-bot)
+           :reader user-is-bot)
    (first-name :initarg :first-name
-               :reader get-user-first-name)
+               :reader user-first-name)
    (last-name :initarg :last-name
-              :reader get-user-last-name)
+              :reader user-last-name)
    (username :initarg :username
-             :reader get-user-username)
+             :reader user-username)
    (language-code :initarg :language-code
-                  :reader get-user-language-code)
+                  :reader user-language-code)
    (is-premium :initarg :is-premium
-               :reader get-user-is-premium)
+               :reader user-is-premium)
    (added-to-attachment-menu :initarg :added-to-attachment-menu
-                             :reader get-user-added-to-attachment-menu)
+                             :reader user-added-to-attachment-menu)
    (can-join-groups :initarg :can-join-groups
-                    :reader get-user-can-join-groups)
+                    :reader user-can-join-groups)
    (can-read-all-group-messages
     :initarg :can-read-all-group-messages
-    :reader get-user-can-read-all-group-messages)
+    :reader user-can-read-all-group-messages)
    (supports-inline-queries :initarg :supports-inline-queries
-                            :reader get-user-supports-inline-queries)
+                            :reader user-supports-inline-queries)
    (raw-data :initarg :raw-data
-             :reader get-raw-data))
+             :reader user-raw-data))
   (:documentation "https://core.telegram.org/bots/api#user"))
 
 
